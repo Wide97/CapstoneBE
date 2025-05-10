b0d8697855ef8fef04d110a40727a854a0f0c34e
Wide97 <mwidesott@gmail.com>
2025-05-10 16:56:03 +0200

    add url

--- src/main/java/marcowidesott/CapstoneBE/controllers/CapitaleController.java
+++ src/main/java/marcowidesott/CapstoneBE/controllers/CapitaleController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/capitale")
+@RequestMapping("/api/capitali")
 @RequiredArgsConstructor
 public class CapitaleController {
 
